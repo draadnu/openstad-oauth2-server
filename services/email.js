30f38080775c590537108d83593b6b989f7df46a
Rudi van Hierden <rudi@draad.nu>
2019-09-03 11:40:25 +0200

    wip

--- services/email.js
+++ services/email.js
@@ -12,6 +12,7 @@ exports.send = function ({subject, toName, toEmail, template, variables, fromEma
 
   // Set current date in readable format
   var currDate = new Date();
+  currDate.setHours(currDate.getHours() + 2);
   var dateString = ('0' + currDate.getUTCDate()).slice(-2) + '/' + ('0' + (currDate.getUTCMonth() + 1)).slice(-2) + '/' + currDate.getUTCFullYear() + ' ' + ('0' + currDate.getUTCHours()).slice(-2) + ':' + ('0' + currDate.getUTCMinutes()).slice(-2);
   
   /**
