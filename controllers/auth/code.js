7c42247807a95fa2cd27649110608bad89d6d16e
Rudi van Hierden <rudi@draad.nu>
2020-06-03 09:05:53 +0200

    Revert "Remove bruteforce reset"
    This reverts commit c896ebf0c46cba880aa60cf490ea5e44e99b7e16.

--- controllers/auth/code.js
+++ controllers/auth/code.js
@@ -44,12 +44,12 @@ exports.postLogin = (req, res, next) => {
       if (err) { return next(err); }
 
       const redirectToAuthorize = () => {
-        //req.brute.reset(() => {
+        req.brute.reset(() => {
           const redirectUrl = req.query.redirect_uri ? encodeURIComponent(req.query.redirect_uri) : req.client.redirectUrl;
           // Redirect if it succeeds to authorize screen
           const authorizeUrl = `/dialog/authorize?redirect_uri=${redirectUrl}&response_type=code&client_id=${req.client.clientId}&scope=offline`;
           return res.redirect(authorizeUrl);
-        //});
+        });
       }
 
       new UserRole({
